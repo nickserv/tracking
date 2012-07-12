8633708f10ef8dea88c4da5b659dfe4463755c0f
Nicolas McCurdy <thenickperson@gmail.com>
2012-07-12 06:58:46 -0400

    revamp tracking.rb to get stuff working again :)

--- lib/tracking.rb
+++ lib/tracking.rb
@@ -1,6 +1,13 @@
-require File.join(File.dirname(__FILE__), "tracking", "list")
-if !File.exists?(File.join(ENV["HOME"], ".tracking"))
-	Dir.mkdir(File.join(ENV["HOME"], ".tracking"))
-	#create config and data files
+require "fileutils"
+
+require File.join(File.dirname(__FILE__), "tracking", "config")
+if not File.exists? File.join(ENV["HOME"], ".tracking")
+	Dir.mkdir File.join(ENV["HOME"], ".tracking")
+	Tracking::Config.write
+	FileUtils.touch File.expand_path Tracking::Config[:data_file]
 end
+
+require File.join(File.dirname(__FILE__), "tracking", "list")
 require File.join(File.dirname(__FILE__), "tracking", "cli")
+
+Tracking::CLI.parse
