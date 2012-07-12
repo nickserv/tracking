080c99ae77daf305ba822e938b4345ed7b2902f8
Nicolas McCurdy <thenickperson@gmail.com>
2012-07-11 23:06:10 -0400

    rename tracking's ruby files

--- lib/tracking.rb
+++ lib/tracking.rb
@@ -1,6 +1,6 @@
-require File.join(File.dirname(__FILE__), "tracking", "tracking")
+require File.join(File.dirname(__FILE__), "tracking", "list")
 if !File.exists?(File.join(ENV["HOME"], ".tracking"))
 	Dir.mkdir(File.join(ENV["HOME"], ".tracking"))
 	#create config and data files
 end
-require File.join(File.dirname(__FILE__), "tracking", "command_line")
+require File.join(File.dirname(__FILE__), "tracking", "cli")
