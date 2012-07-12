2a0a3325e9099d33051d1f73ede1c78e477357bd
Nicolas McCurdy <thenickperson@gmail.com>
2012-07-12 01:58:34 -0400

    organize ruby files under the Tracking module and remove #!s

--- lib/tracking/list.rb
+++ lib/tracking/list.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 #Tracking's core.
 
 #imports
@@ -8,75 +7,79 @@ require "time"
 #model/controller module methods
 module Tracking
 
-	$config = YAML.load_file(ENV["HOME"] + "/.tracking/config.yml")
-	$config[:data_file] = File.expand_path($config[:data_file])
+	module List
 
-	$data_file = File.new($config[:data_file])
+		$config = YAML.load_file(ENV["HOME"] + "/.tracking/config.yml")
+		$config[:data_file] = File.expand_path($config[:data_file])
 
-	#adds an item to the list
-	def self.add item
-		newline = "\n"
-		date = Time.now.to_s
-		$data_file.write(newline+date+"|"+item.chomp)
-	end
+		$data_file = File.new($config[:data_file])
 
-	#removes an item from the list
-	def self.remove
-		lines = $data_file.readlines
-		lines.pop
-		$data_file.write do |f|
-			lines.each do |line|
-				if line == lines.last
-					line.chomp!
+		#adds an item to the list
+		def self.add item
+			newline = "\n"
+			date = Time.now.to_s
+			$data_file.write(newline+date+"|"+item.chomp)
+		end
+
+		#removes an item from the list
+		def self.remove
+			lines = $data_file.readlines
+			lines.pop
+			$data_file.write do |f|
+				lines.each do |line|
+					if line == lines.last
+						line.chomp!
+					end
+					f.print line
 				end
-				f.print line
 			end
 		end
-	end
 
-	#clears the entire list
-	def self.clear
-		$data_file.write do |f|
-			f.write ""
-		end
-	end
-
-	#opens the list data file in a text editor
-	def self.edit
-		system ENV["EDITOR"] + " " + $config[:data_file]
-	end
-
-end
-
-#gets and formats the amount of time passed between two times
-def get_elapsed_time(time1, time2)
-	#calculate the elapsed time and break it down into different units
-	seconds = (time2 - time1).floor
-	if seconds >= 60
-		minutes = seconds / 60
-		seconds = seconds % 60
-		if minutes >= 60
-			hours = minutes / 60
-			minutes = minutes % 60
-			if hours >= 24
-				days = hours / 24
-				hours = hours % 24
+		#clears the entire list
+		def self.clear
+			$data_file.write do |f|
+				f.write ""
 			end
 		end
+
+		#opens the list data file in a text editor
+		def self.edit
+			system ENV["EDITOR"] + " " + $config[:data_file]
+		end
+
 	end
-	#return a string of the formatted elapsed time
-	elapsed = ""
-	if days
-		elapsed += "#{days.to_s}d "
+
+	#gets and formats the amount of time passed between two times
+	def get_elapsed_time(time1, time2)
+		#calculate the elapsed time and break it down into different units
+		seconds = (time2 - time1).floor
+		if seconds >= 60
+			minutes = seconds / 60
+			seconds = seconds % 60
+			if minutes >= 60
+				hours = minutes / 60
+				minutes = minutes % 60
+				if hours >= 24
+					days = hours / 24
+					hours = hours % 24
+				end
+			end
+		end
+		#return a string of the formatted elapsed time
+		elapsed = ""
+		if days
+			elapsed += "#{days.to_s}d "
+		end
+		if hours
+			elapsed += "#{hours.to_s}h "
+		end
+		if minutes
+			elapsed += "#{minutes.to_s}m "
+		end
+		if seconds
+			elapsed += "#{seconds.to_s}s"
+		end
+		return elapsed
 	end
-	if hours
-		elapsed += "#{hours.to_s}h "
-	end
-	if minutes
-		elapsed += "#{minutes.to_s}m "
-	end
-	if seconds
-		elapsed += "#{seconds.to_s}s"
-	end
-	return elapsed
+
 end
