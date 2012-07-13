f4e6c683437f8d2eecd3b2578397b7088e7bec2b
Nicolas McCurdy <thenickperson@gmail.com>
2012-07-13 05:47:01 -0400

    fix #44

--- lib/tracking.rb
+++ lib/tracking.rb
@@ -1,9 +1,19 @@
 require "fileutils"
 
 require File.join(File.dirname(__FILE__), "tracking", "config")
-if not File.exists? File.join(ENV["HOME"], ".tracking")
+
+#create ~/.tracking
+if not File.exist? File.join(ENV["HOME"], ".tracking")
 	Dir.mkdir File.join(ENV["HOME"], ".tracking")
+end
+
+#create config file
+if not File.exist? File.join(ENV["HOME"], ".tracking", "config.yml")
 	Tracking::Config.write
+end
+
+#create data file
+if not File.exist? File.join(File.expand_path Tracking::Config[:data_file])
 	FileUtils.touch File.expand_path Tracking::Config[:data_file]
 end
 
