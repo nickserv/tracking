2e20432ce563dc0560ca7052f3f2bc4f309f3b54
Nicolas McCurdy <thenickperson@gmail.com>
2012-07-24 04:39:12 -0400

    Add a (slightly) legit test for different things in tracking's CLI

--- test/helper.rb
+++ test/helper.rb
@@ -15,4 +15,16 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'tracking'
 
 class Test::Unit::TestCase
+
+	def capture_output &block
+		original_stdout = $stdout
+		$stdout = fake = StringIO.new
+		begin
+			yield
+		ensure
+			$stdout = original_stdout
+		end
+		fake.string
+	end
+
 end
---
+++ test/test_cli.rb
@@ -0,0 +1,39 @@
+require_relative 'helper'
+
+class TestCLI < Test::Unit::TestCase
+	context "Tracking's CLI" do
+
+		should "clear list (to prepare for other tests)" do
+			capture_output { Tracking::List.clear }
+		end
+
+		should "display empty list" do
+			capture_output { Tracking::CLI.display }
+		end
+
+		should "add a task" do
+			capture_output { Tracking::List.add "first task" }
+		end
+
+		should "add another task" do
+			capture_output { Tracking::List.add "second task" }
+		end
+
+		should "display list with two items" do
+			capture_output { Tracking::CLI.display }
+		end
+
+		should "delete the second task" do
+			capture_output { Tracking::List.delete }
+		end
+
+		should "clear list" do
+			capture_output { Tracking::List.clear }
+		end
+
+		should "display help information (run from the system's shell)" do
+			capture_output { `#{File.expand_path("../bin/tracking")} --help` }
+		end
+
+	end
+end
