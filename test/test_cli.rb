8165cec495a548569e039d502e916b48717377c4
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-03 17:36:33 -0400

    Test tracking commands directly through ruby, not through the shell
    This is a temporary fix, as running directly through the shell hides
    exceptions and causes tests to result in false positives.

--- test/test_cli.rb
+++ test/test_cli.rb
@@ -5,31 +5,38 @@ class TestCLI < Test::Unit::TestCase
 	context "Tracking's CLI" do
 
 		should "clear list (to prepare for other tests)" do
-			test_command "-c"
+			#test_command "-c"
+			capture_output { Tracking::List.clear }
 		end
 
 		should "display empty list" do
-			test_command
+			#test_command
+			capture_output { Tracking::CLI.display }
 		end
 
 		should "add a task" do
-			test_command "first task"
+			#test_command "first task"
+			capture_output { Tracking::List.add "first task" }
 		end
 
 		should "add another task" do
-			test_command "second task"
+			#test_command "second task"
+			capture_output { Tracking::List.add "second task" }
 		end
 
 		should "display list with two items" do
-			test_command
+			#test_command
+			capture_output { Tracking::CLI.display }
 		end
 
 		should "delete the second task" do
-			test_command "-d"
+			#test_command "-d"
+			capture_output { Tracking::List.delete }
 		end
 
 		should "clear list" do
-			test_command "-c"
+			#test_command "-c"
+			capture_output { Tracking::List.clear }
 		end
 
 		should "display help information (run from the system's shell)" do
