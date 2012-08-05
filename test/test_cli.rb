459a5be5945f3ac7ad3a71639b820c6e28e08f86
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-05 17:14:02 -0400

    Consolidate tests (fix #89)

--- test/test_cli.rb
+++ test/test_cli.rb
@@ -1,40 +1,36 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'helper'
+require 'fileutils'
 
 class TestCLI < Test::Unit::TestCase
 	context 'Tracking\'s CLI' do
 
-		should 'clear list (to prepare for other tests)' do
+		def setup
+			FileUtils.cd File.expand_path('~/.tracking')
+			FileUtils.mkdir 'test_backup'
+			FileUtils.mv %w(config.yml data.csv), 'test_backup'
+		end
+
+		def teardown
+			FileUtils.cd File.expand_path('~/.tracking/test_backup')
+			FileUtils.mv %w(config.yml data.csv), File.expand_path('..')
+			FileUtils.cd File.expand_path('..')
+			FileUtils.rmdir 'test_backup'
+		end
+
+		should 'perform a few operations on a new list and then clear it' do
 			#test_command '-c'
 			capture_output { Tracking::List.clear }
-		end
-
-		should 'display empty list' do
 			#test_command
 			capture_output { Tracking::CLI.display }
-		end
-
-		should 'add a task' do
 			#test_command 'first task'
 			capture_output { Tracking::List.add 'first task' }
-		end
-
-		should 'add another task' do
 			#test_command 'second task'
 			capture_output { Tracking::List.add 'second task' }
-		end
-
-		should 'display list with two items' do
 			#test_command
 			capture_output { Tracking::CLI.display }
-		end
-
-		should 'delete the second task' do
 			#test_command '-d'
 			capture_output { Tracking::List.delete }
-		end
-
-		should 'clear list' do
 			#test_command '-c'
 			capture_output { Tracking::List.clear }
 		end
