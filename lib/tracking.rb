46d58e3be1c5b45b2b3775592312ef277f95ac59
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-05 02:32:09 -0400

    Tweak and complete comments in tracking.rb

--- lib/tracking.rb
+++ lib/tracking.rb
@@ -1,21 +1,24 @@
+# Require file utilities
 require 'fileutils'
 
+# Require Tracking::Config
 require File.join(File.dirname(__FILE__), 'tracking', 'config')
 
-#create ~/.tracking
+# Create ~/.tracking
 if not File.exist? File.join(ENV['HOME'], '.tracking')
 	Dir.mkdir File.join(ENV['HOME'], '.tracking')
 end
 
-#create config file
+# Create config file
 if not File.exist? File.join(ENV['HOME'], '.tracking', 'config.yml')
 	Tracking::Config.write
 end
 
-#create data file
+# Create data file
 if not File.exist? File.expand_path Tracking::Config[:data_file]
 	FileUtils.touch File.expand_path Tracking::Config[:data_file]
 end
 
+# Require the rest of tracking
 require File.join(File.dirname(__FILE__), 'tracking', 'list')
 require File.join(File.dirname(__FILE__), 'tracking', 'cli')
