89fb3703384fa0e442c75d946ef4a9f5f9191ebe
Nicolas McCurdy <thenickperson@gmail.com>
2012-09-10 01:18:28 -0400

    Port tests to Rspec (fix #94)

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,49 +1,46 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'helper'
 require 'fileutils'
 
-class TestCLI < Test::Unit::TestCase
-	context 'Tracking\'s CLI' do
+describe Tracking::CLI do
 
-		def setup
-			FileUtils.cd File.expand_path('~/.tracking')
-			FileUtils.mkdir 'test_backup'
-			%w(config.yml data.csv).each do |f|
-				FileUtils.mv(f, 'test_backup') if File.exist? f
-			end
+	before :each do
+		FileUtils.cd File.expand_path('~/.tracking')
+		FileUtils.mkdir 'test_backup'
+		%w(config.yml data.csv).each do |f|
+			FileUtils.mv(f, 'test_backup') if File.exist? f
 		end
-
-		def teardown
-			FileUtils.cd File.expand_path('~/.tracking/test_backup')
-			%w(config.yml data.csv).each do |f|
-				FileUtils.mv(f, File.expand_path('..')) if File.exist? f
-			end
-			FileUtils.cd File.expand_path('..')
-			FileUtils.rmdir 'test_backup'
-		end
-
-		should 'perform a few operations on a new list and then clear it' do
-			#test_command '-c'
-			capture_output { Tracking::List.clear }
-			#test_command
-			capture_output { Tracking::CLI.display }
-			#test_command 'first task'
-			capture_output { Tracking::List.add 'first task' }
-			#test_command 'second task'
-			capture_output { Tracking::List.add 'second task' }
-			#test_command '-r second task, renamed'
-			capture_output { Tracking::List.rename 'second task, renamed' }
-			#test_command
-			capture_output { Tracking::CLI.display }
-			#test_command '-d'
-			capture_output { Tracking::List.delete }
-			#test_command '-c'
-			capture_output { Tracking::List.clear }
-		end
-
-		should 'display help information (run from the system\'s shell)' do
-			test_command '-h'
-		end
-
 	end
+
+	after :each do
+		FileUtils.cd File.expand_path('~/.tracking/test_backup')
+		%w(config.yml data.csv).each do |f|
+			FileUtils.mv(f, File.expand_path('..')) if File.exist? f
+		end
+		FileUtils.cd File.expand_path('..')
+		FileUtils.rmdir 'test_backup'
+	end
+
+	it 'performs a few operations on a new list and then clears it' do
+		#test_command '-c'
+		capture_output { Tracking::List.clear }
+		#test_command
+		capture_output { Tracking::CLI.display }
+		#test_command 'first task'
+		capture_output { Tracking::List.add 'first task' }
+		#test_command 'second task'
+		capture_output { Tracking::List.add 'second task' }
+		#test_command '-r second task, renamed'
+		capture_output { Tracking::List.rename 'second task, renamed' }
+		#test_command
+		capture_output { Tracking::CLI.display }
+		#test_command '-d'
+		capture_output { Tracking::List.delete }
+		#test_command '-c'
+		capture_output { Tracking::List.clear }
+	end
+
+	it 'displays help information (run from the system shell)' do
+		test_command '-h'
+	end
+
 end
--- spec/helper.rb
+++ spec/helper.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'bundler'
 begin
   Bundler.setup(:default, :development)
@@ -7,33 +6,25 @@ rescue Bundler::BundlerError => e
   $stderr.puts 'Run `bundle install` to install missing gems'
   exit e.status_code
 end
-require 'test/unit'
-require 'shoulda'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'tracking'
+require_relative '../lib/tracking'
 
-class Test::Unit::TestCase
+@tracking_path = File.join(File.dirname(__FILE__), '..', 'bin', 'tracking')
+@tracking_command = "ruby #{@tracking_path}"
 
-	@tracking_path = File.join(File.dirname(__FILE__), '..', 'bin', 'tracking')
-	@tracking_command = "ruby #{@tracking_path}"
-
-	def capture_output &block
-		original_stdout = $stdout
-		$stdout = fake = StringIO.new
-		begin
-			yield
-		ensure
-			$stdout = original_stdout
-		end
-		fake.string
+def capture_output &block
+	original_stdout = $stdout
+	$stdout = fake = StringIO.new
+	begin
+		yield
+	ensure
+		$stdout = original_stdout
 	end
+	fake.string
+end
 
-	def test_command args=''
-		capture_output do
-			system "#{@tracking_command} #{args}"
-		end
+def test_command args=''
+	capture_output do
+		system "#{@tracking_command} #{args}"
 	end
-
 end
