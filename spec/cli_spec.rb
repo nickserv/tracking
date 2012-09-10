17a5d6a18165516fbc4eff92ba2a46d43ea69466
Nicolas McCurdy <thenickperson@gmail.com>
2012-09-10 01:41:26 -0400

    Move commented CLI test stuff into a single comment block

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -21,23 +21,28 @@ describe Tracking::CLI do
 	end
 
 	it 'performs a few operations on a new list and then clears it' do
-		#test_command '-c'
 		capture_output { Tracking::List.clear }
-		#test_command
 		capture_output { Tracking::CLI.display }
-		#test_command 'first task'
 		capture_output { Tracking::List.add 'first task' }
-		#test_command 'second task'
 		capture_output { Tracking::List.add 'second task' }
-		#test_command '-r second task, renamed'
 		capture_output { Tracking::List.rename 'second task, renamed' }
-		#test_command
 		capture_output { Tracking::CLI.display }
-		#test_command '-d'
 		capture_output { Tracking::List.delete }
-		#test_command '-c'
 		capture_output { Tracking::List.clear }
 	end
+
+=begin
+	it 'performs a few operations on a new list and then clears it' do
+		test_command '-c'
+		test_command
+		test_command 'first task'
+		test_command 'second task'
+		test_command '-r second task, renamed'
+		test_command
+		test_command '-d'
+		test_command '-c'
+	end
+=end
 
 	it 'displays help information (run from the system shell)' do
 		test_command '-h'
