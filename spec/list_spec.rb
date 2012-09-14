4c9a3be6e4b1306664309c918e17a235c6e1cc67
Nicolas McCurdy <thenickperson@gmail.com>
2012-09-14 01:13:35 -0400

    Add list spec

---
+++ spec/list_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Tracking::List do
+
+	before(:all) { backup_data }
+	after(:all)  { restore_data }
+
+	it 'performs a few operations on a new list and then clears it' do
+		capture_output do
+			Tracking::List.clear
+			Tracking::List.get.length.should == 0
+			Tracking::List.add 'first task'
+			Tracking::List.add 'second task'
+			Tracking::List.get.length.should == 2
+			Tracking::List.rename 'second task, renamed'
+			Tracking::List.delete
+			Tracking::List.get.length.should == 1
+			Tracking::List.clear
+			Tracking::List.get.length.should == 0
+		end
+	end
+
+end
