88f15c4949bbb60c1f027341941e5de8e0ab840c
Nicolas McCurdy <thenickperson@gmail.com>
2012-09-14 00:00:19 -0400

    Add config spec

---
+++ spec/config_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Tracking::CLI do
+
+	before(:all) { backup_data }
+	after(:all)  { restore_data }
+
+	it 'reads a value from the config file' do
+		Tracking::Config[:lines].should == Tracking::Config.defaults[:lines]
+	end
+
+	it 'writes a value to the config file' do
+		Tracking::Config[:lines] = 9001
+		Tracking::Config[:lines].should == 9001
+		File.exist?(Tracking::Config::PATH).should be_true
+	end
+
+	it 'creates a config file' do
+		File.exist?(Tracking::Config::PATH).should be_true
+	end
+
+end
