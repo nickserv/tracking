8dda4d109467fe90585060a20459e2d5b2c5e6e9
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-27 16:51:54 -0500

    Require everything in lib/tracking automatically

--- lib/tracking.rb
+++ lib/tracking.rb
@@ -1,9 +1,6 @@
 # Require all of tracking's stuff
-lib_path = File.join(File.dirname(__FILE__), 'tracking')
-require File.join(lib_path, 'config')
-require File.join(lib_path, 'list')
-require File.join(lib_path, 'task')
-require File.join(lib_path, 'cli')
+lib_path = File.join(File.dirname(__FILE__), 'tracking', '*.rb')
+Dir[lib_path].each { |file| require file }
 
 # Tracking is the main namespace that all of the other modules and classes are a
 # part of
